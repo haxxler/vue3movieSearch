622ad8f8b83d0a1f59a09f3b4f4fa45582bc70b9
haxxler <haxxkim@gmail.com>
2022-08-24 15:12:17 +0900

    Vue.js bootstrap adap test is completed!

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,4 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Home!!</h1>                                                                                                                                                                                                                                                                                                        
+  <h1>Home!!</h1>
+  <div class="btn btn-primary">
+    Home
+  </div>                                                                                                                                                                                                                                                                                                        
 </template>
