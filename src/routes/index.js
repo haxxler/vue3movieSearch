45a1fc7e69288943f0b864c00de94c0057b08771
haxxler <haxxkim@gmail.com>
2022-08-24 16:00:15 +0900

    Header nav section practice is completed!!

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './Home'
 import About from './About'
+import Movie from './Movie'
 
 export default createRouter({
   // Hash
@@ -12,6 +13,10 @@ export default createRouter({
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/movie',
+      component: Movie
     },
     {
       path: '/about',
